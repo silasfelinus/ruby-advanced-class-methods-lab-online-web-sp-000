c3d51c45ef3839547bf36ac57b2c19a278cef540
Silas Michael Knight <silasfelinus@gmail.com>
2020-11-29 22:15:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,4 +33,10 @@ end
 def self.find_by_name(name)
   self.all.find{|song| song.name == name}
 end
+
+def self.find_or_create_by_name(name)
+  if self.find_by_name(name) == nil
+    self.create_by_name(name)
+  else self.find_by_name(name)
+
 end
