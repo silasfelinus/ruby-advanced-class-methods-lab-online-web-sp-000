2f5e4e9ae93146bfaf9d091c3e28c5e232dacee5
Silas Michael Knight <silasfelinus@gmail.com>
2020-11-30 00:09:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,7 +47,7 @@ class Song
   end
 
   def self.new_from_filename(filename)
-    self.new_by_name(filename.split("-")[1])
+    self.new_by_name(filename.split("-")[1].strip)
   end
 
   def self.create_from_filename(filename)
